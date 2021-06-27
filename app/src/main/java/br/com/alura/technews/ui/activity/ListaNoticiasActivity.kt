718642b080a6eb3c6721f49ce09efffd62eb067e
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-06-26 21:41:54 -0300

    implement the first fragment

---
+++ app/src/main/java/br/com/alura/technews/ui/activity/ListaNoticiasActivity.kt
@@ -0,0 +1,40 @@
+package br.com.alura.technews.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
+import br.com.alura.technews.R
+import br.com.alura.technews.model.Noticia
+import br.com.alura.technews.ui.activity.extensions.mostraErro
+import br.com.alura.technews.ui.recyclerview.adapter.ListaNoticiasAdapter
+import br.com.alura.technews.ui.viewmodel.ListaNoticiasViewModel
+import kotlinx.android.synthetic.main.activity_lista_noticias.*
+import org.koin.android.viewmodel.ext.android.viewModel
+
+private const val TITULO_APPBAR = "Notícias"
+
+class ListaNoticiasActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_noticias)
+        title = TITULO_APPBAR
+    }
+
+
+    private fun abreFormularioModoCriacao() {
+        val intent = Intent(this, FormularioNoticiaActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun abreVisualizadorNoticia(it: Noticia) {
+        val intent = Intent(this, VisualizaNoticiaActivity::class.java)
+        intent.putExtra(NOTICIA_ID_CHAVE, it.id)
+        startActivity(intent)
+    }
+
+}
